4eec3c8e46846a926093ed16172114df1c54efdd
Abu Saleh <abusalehmdtayeen@gmail.com>
2019-03-13 13:36:58 -0600

    removed eol traits test

--- tests/test_get_traits.py
+++ tests/test_get_traits.py
@@ -3,14 +3,14 @@ import json
 import phylotastic_services
 
 class TestGetTraits(TestCase):
-    def test_get_traits_EOL(self):
-        """
-        Testing getting habitat and conservation status of a list of species from EOL traitsbank
-        """
-        result = phylotastic_services.get_traits_EOL(["Balaenoptera musculus"])
+    #def test_get_traits_EOL(self):
+    #    """
+    #    Testing getting habitat and conservation status of a list of species from EOL traitsbank
+    #    """
+    #    result = phylotastic_services.get_traits_EOL(["Balaenoptera musculus"])
         # Check whether result is what it should be according to docs
-        self.assertTrue(u'oceanic zone' in json.loads(result)['species'][0]['habitats'])
-        self.assertTrue(u'endangered' in json.loads(result)['species'][0]['conservation_status'])
+    #    self.assertTrue(u'oceanic zone' in json.loads(result)['species'][0]['habitats'])
+    #    self.assertTrue(u'endangered' in json.loads(result)['species'][0]['conservation_status'])
         
 
     def test_get_traits_ECOS(self):
